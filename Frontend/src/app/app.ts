b102a5878bef64c5ce739ef018846351274ad78b
snirpro <Snirpr00@gmail.com>
2025-11-20 19:00:18 +0200

    frontend skelaton

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { SearchBar } from './components/search-bar/search-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, SearchBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
